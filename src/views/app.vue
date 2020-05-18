2f5a690582d1b71b9f5c0cfa2a20896a94095867
piecego <wktrf@foxmail.com>
2020-05-18 13:12:22 +0800

    初始化app.vue及样式

---
+++ src/views/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <main id="app">
+    <router-view />
+  </main>
+</template>
+<style lang="less">
+@import '~assets/styles/initial';
+@import '~assets/styles/variables';
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Josefin Sans', 'Sarasa Mono T SC', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  .variables();
+}
+</style>
